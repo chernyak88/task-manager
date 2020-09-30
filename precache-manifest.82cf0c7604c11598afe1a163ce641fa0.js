4b5f25e52cf50ce4e57bba0e3516e9e3f74b133b
Igor Chernyak <i_chernyak@inbox.ru>
2020-09-30 12:16:55 +0300

    Updates

---
+++ precache-manifest.82cf0c7604c11598afe1a163ce641fa0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9427ff116aa67d35b816be39584255",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "e351e2587ce68bd5eebd",
+    "url": "/task-manager/static/css/main.4a9cf89a.chunk.css"
+  },
+  {
+    "revision": "2c1e46637383627b6f30",
+    "url": "/task-manager/static/js/2.8de31858.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/task-manager/static/js/2.8de31858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e351e2587ce68bd5eebd",
+    "url": "/task-manager/static/js/main.567bf668.chunk.js"
+  },
+  {
+    "revision": "6f879587e75cb206c02c",
+    "url": "/task-manager/static/js/runtime-main.97270499.js"
+  },
+  {
+    "revision": "b8681d94fe7fd3c3ae18d4cdbfca802a",
+    "url": "/task-manager/static/media/delete.b8681d94.svg"
+  },
+  {
+    "revision": "a554211b2dbeae4e7e8b412b3d1740a4",
+    "url": "/task-manager/static/media/edit.a554211b.svg"
+  }
+]);
